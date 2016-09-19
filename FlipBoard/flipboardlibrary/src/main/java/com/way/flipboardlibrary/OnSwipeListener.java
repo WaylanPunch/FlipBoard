69722fc479f9cbfe3859a8d82918cbaa14d456e3
waylanpunch <waylanpunch@gmail.com>
2016-09-20 01:49:05 +0800

    A Flip Board Demo

---
+++ FlipBoard/flipboardlibrary/src/main/java/com/way/flipboardlibrary/OnSwipeListener.java
@@ -0,0 +1,15 @@
+package com.way.flipboardlibrary;
+
+/**
+ * Created by pc on 2016/9/19.
+ */
+
+public interface OnSwipeListener {
+    void onSwipeLeft();
+
+    void onSwipeRight();
+
+    void onSwipeUp();
+
+    void onSwipeDown();
+}
